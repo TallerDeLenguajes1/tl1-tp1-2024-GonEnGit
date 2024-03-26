b0ff363d16155179ca1fab66c08cf0fc106d63c6
GonzaloMartinez <goncham94@gmail.com>
2024-03-26 15:58:28 -0300

    punto 3f

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    printf("Hola, Mundo");
+
+    int numero = 21;
+    int *puntero;
+    puntero = &numero;
+
+    printf("\nHola, Mundo\n");
+
+    printf("Contenido del puntero: %d\n", *puntero);
+    printf("Direccion en el puntero: %p\n", puntero);
+    printf("Direccion de la variable: %p\n", &numero);
+    printf("Direccion del puntero: %p\n", &puntero);
+    printf("Tama%co del puntero: %d bits\n", 164, sizeof(puntero));
+
+
     return 0;
 }
