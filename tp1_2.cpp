369d64041c0b4a80089c73070dfa09dc1e9cbdd4
GonzaloMartinez <goncham94@gmail.com>
2024-03-26 16:47:56 -0300

    punto 4 sin terminar

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -1,22 +1,30 @@
 #include <stdio.h>
 
 /* Prototipos */
-int Cuadrados(int x);
+int Cuadrados(int X);
+void CuadradosVoid(int *solucion, int Y);
 
 int main(int argc, char *argv[]) {
 
+    // declaracion de variables
+    int valor = 7;
     int resultado;
+    int *solucion;
 
-    resultado = Cuadrados(5);
+    resultado = Cuadrados(valor);
 
     printf("%d", resultado);
+    printf("%d", *solucion);
 
     return 0;
 }
 
 /* Desarrollo de funciones */
-int Cuadrados(int x) {
+int Cuadrados(int X) {
     int resultado;
+    return resultado = X * X;
+}
 
-    return resultado = x * x;
+void CradradosVoid(int *solucion, int Y) {
+    *solucion = Y * Y;
 }
